979beb3c0adf4c91e57365a239f90123c261500d
Jo Hyeon Ah <gusdk905@naver.comm>
2017-10-05 13:44:53 +0900

    초기 환경 세팅

---
+++ app/src/main/java/com/example/user/myapplication/MenuHelpLayout.java
@@ -0,0 +1,23 @@
+package com.example.user.myapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by user on 2017-09-28.
+ */
+
+public class MenuHelpLayout extends Fragment {
+
+    View v;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        v= inflater.inflate(R.layout.menu_help, container, false);
+        return v;
+    }
+}
